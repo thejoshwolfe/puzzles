89f3586cbe73e8b6f33dca6c5a0ad85f6f227005
Josh Wolfe <thejoshwolfe@gmail.com>
2013-01-12 12:50:29 -0700

    cipher -v prints percent progress, and can tolerate immediately impossible words

--- cipher.py
+++ cipher.py
@@ -15,42 +15,83 @@ options, args = parser.parse_args()
 if args == ["-"]:
     args = [sys.stdin.read().strip()]
 
-def verbose(message):
-    if options.verbose:
-        print(message)
-verbose("reading dictionary")
+last_progress_line = ""
+def clear_progress():
+    if not options.verbose: return
+    sys.stdout.write("\r")
+def restore_progress():
+    sys.stdout.write(last_progress_line)
+    sys.stdout.flush()
+def show_progress(out_of_1):
+    if not options.verbose: return
+    line = "{0:.1f}%".format(out_of_1*100)
+    global last_progress_line
+    if line == last_progress_line: return
+    clear_progress()
+    sys.stdout.write(line)
+    sys.stdout.flush()
+    last_progress_line = line
+def say(message):
+    clear_progress()
+    message += " " * (len(last_progress_line) - len(message))
+    print(message)
+    restore_progress()
+
 dictionary = open(options.dictionary).read().split("\n")
 dictionary = [word.lower() for word in dictionary if re.match("^[A-Za-z]+$", word)]
 
 def generate_sentence(word_count):
-    return [dictionary[random.randint(0, len(dictionary)-1)] for _ in range(word_count)]
+    sentence = " ".join(dictionary[random.randint(0, len(dictionary)-1)] for _ in range(word_count))
+    sentence += "."
+    if options.verbose:
+        sys.stderr.write(sentence + "\n")
+    return sentence
 def scramble(sentence):
-    key = list(string.lowercase)
-    random.shuffle(key)
-    scrambled = ["".join(key[ord(c)-ord("a")] for c in word) for word in sentence]
-    sys.stderr.write(" ".join(sentence) + "\n")
-    print(" ".join(scrambled))
+    words = split(sentence)
+    order = list(string.lowercase)
+    random.shuffle(order)
+    key = dict(zip(string.lowercase, order))
+    scrambled = "".join(key.get(c, c) for c in sentence)
+    print(scrambled)
+
+def split(sentence):
+    return re.sub("[^a-z ]", "", sentence).split()
 
 def solve(original_scrambled):
     original_scrambled = original_scrambled.lower()
-    scrambled = re.sub("[^a-z ]", "", original_scrambled).split()
+    scrambled = split(original_scrambled)
     by_signature = collections.defaultdict(list)
-    verbose("building signature database")
     def get_signature(word):
         letter_index = collections.defaultdict(itertools.count().next)
         return tuple(letter_index[c] for c in word)
     for word in dictionary:
         by_signature[get_signature(word)].append(word)
 
+    # starting with words with the most letters results in smoother progress
+    if options.verbose:
+        scrambled.sort(key=lambda word: -max(get_signature(word)))
     partial_keys_list = [[dict(zip(scrambled_word, real_word)) for real_word in by_signature[get_signature(scrambled_word)]] for scrambled_word in scrambled]
-    # start with the ones with the fewest possibilites
-    partial_keys_list.sort(key=len)
-    verbose("difficulty: {0}".format(len(partial_keys_list[0])))
+    # starting with words with the fewest possibilites results in fastest solve time
+    if not options.verbose:
+        partial_keys_list.sort(key=len)
+    # this is the test case used to measure performance:
+    # "the time has come the walrus said to talk of many things.  of shores and ships and sealing wax, of cabbages and kings.  carroll"
 
-    def recurse(parent_key, index):
+    # eliminate any impossible words
+    compromized_indexes = set(i for (i, partial_keys) in enumerate(partial_keys_list) if len(partial_keys) == 0)
+    # this test case has immediately impossible words:
+    # "when on a music staff, ff for fortissimo means very loud, much like mournful howls voiced by frustrated solvers."
+
+    def recurse(parent_key, index, significance):
+        if index in compromized_indexes:
+            # pass through
+            for result in recurse(parent_key, index+1, significance):
+                yield result
+            return
         if index == len(partial_keys_list):
             yield parent_key
             return
+        progress_step = significance / len(partial_keys_list[index])
         for partial_key in partial_keys_list[index]:
             combined_key = dict(parent_key.items())
             def is_possible():
@@ -69,27 +110,40 @@ def solve(original_scrambled):
                     combined_key[k] = v
                 return True
             if not is_possible():
+                show_progress(progress[0])
+                progress[0] += progress_step
                 continue
-            for result in recurse(combined_key, index+1):
+            for result in recurse(combined_key, index+1, progress_step):
                 yield result
-    answer_keys = recurse({}, 0)
-
+    def announce_compromizes():
+        if not options.verbose: return
+        if not compromized_indexes: return
+        compromizes = len(compromized_indexes)
+        say("leaving out {0} word{1}.".format(compromizes, "s"[0:bool(compromizes - 1)]))
     answer_set = set()
-    for answer_key in answer_keys:
-        answer = "".join(answer_key.get(c, c) for c in original_scrambled)
-        if answer not in answer_set:
-            print(answer)
-            answer_set.add(answer)
+    while len(compromized_indexes) < len(partial_keys_list):
+        progress = [0]
+        announce_compromizes()
+        answer_keys = recurse({}, 0, 1.0)
 
-    sys.exit(int(not answer_set))
+        for answer_key in answer_keys:
+            answer = "".join(answer_key.get(c, c) for c in original_scrambled)
+            if answer not in answer_set:
+                say(answer)
+                answer_set.add(answer)
+
+        if answer_set:
+            clear_progress()
+            break
+        else:
+            # try leaving out random words
+            TODO
 
 if options.generate:
-    try:
-        word_count = int(" ".join(args))
-    except ValueError:
-        sentence = args
-    else:
-        sentence = generate_sentence(word_count)
+    sentence = " ".join(args)
+    try: word_count = int(sentence)
+    except ValueError: pass
+    else: sentence = generate_sentence(word_count)
     scramble(sentence)
 else:
     solve(" ".join(args))
